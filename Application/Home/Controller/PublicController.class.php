5bc0e632ea423fab8fb5cab46baf7ac19cf0b002
namexd <namexd@163.com>
2017-04-21 15:13:14 +0800

    登陆修改

--- Application/Home/Controller/PublicController.class.php
+++ Application/Home/Controller/PublicController.class.php
@@ -6,23 +6,26 @@
  * Time: 11:16
  */
 
-namespace Weixin\Controller;
+namespace Home\Controller;
 
 use Think\Controller;
 use Think\Model;
 
 class PublicController extends Controller
 {
+//    登陆
     public function login()
     {
-        if (isset($_SESSION["user_type"])){
-            if ($_SESSION["user_type"]==1){
-                $this->redirect("Student/index");
-            }else if ($_SESSION["user_type"]==2){
-                $this->redirect("Teacher/index");
-            }
+        $username = I("post.username");
+        $passwd = I("post.password");
+        if ($username=='admin' && $passwd=='123456'){
+            $user=array('userid'=>1,'user_type'=>2);
+            session("user",$user);
+            $this->ajaxReturn(array("status"=>1,"info"=>'登陆成功'));
+    }else{
+            $this->ajaxReturn(array("status"=>0,"info"=>'账号或密码错误！'));
         }
-        $url=I('url');
+        $url=get_refer_url();
         if($url){
             $this->assign('url',$url);
         }
@@ -309,34 +312,6 @@ class PublicController extends Controller
         $this->assign('country_list',$country_list);
         $this->display();
     }
-
-    public function region_tree(){
-        if(IS_AJAX){
-            $code=I('get.region_code');
-            $regionMdl=M('class_region');
-            if($code<100){
-                $regioninfo[]=$regionMdl->where("father_id= '$code'")->select();
-                $regioninfo[]=$regionMdl->where("father_id=".$regioninfo[0][0]['region_code'])->select();
-                $this->ajaxReturn($regioninfo);
-            }
-            $regioninfo=$regionMdl->where("father_id= '$code'")->select();
-            $this->ajaxReturn($regioninfo);
-        }
-    }
-    public function region_school(){
-        if(IS_AJAX){
-            $code=I('get.region_code');
-            $schoolMdl=M('class_school_users');
-            $school_info=$schoolMdl->where("areacode='$code'")->field('school_name,id')->select();
-            if(!$school_info){
-                $school_info=0;
-            }
-            $this->ajaxReturn($school_info);
-        }
-    }
-
-
-
     //忘记密码
     public function forget(){
         if($_POST){
@@ -376,141 +351,13 @@ class PublicController extends Controller
             }
         }
     }
-    public function wx_jump(){
-        $openid=$_GET['openid'];
-        $identify=$_GET['identify'];
-        if($identify=='student'){
-            $users=M('365class.class_student_users');
-        }elseif($identify=='teacher'){
-            $users=M('365class.class_teacher_users');
+//    检测用户是否登陆并返回用户信息
+    public function is_login(){
+        if(check_login()){
+            $user_info = array('userid'=>get_uid(),'user_type'=>get_user_type());
+            $this->ajaxReturn(array("status"=>1,"user"=>$user_info));
+        }else{
+            $this->ajaxReturn(array("status"=>0,"info"=>"此用户未登录！"));
         }
-        $result=$users->where(array('openid'=>$openid))->find();
-//        dump($result);
-        if($result==false){
-            $url=U('Public/wx_bind',array('identify'=>$identify,'openid'=>$openid));
-            $this->show("<script>alert('请先绑定已注册账号');window.location.href='{$url}';</script>");
-        }elseif($result !=false || count($result)==1){
-            //记录cookie
-            $salt = $this->random_str(16);
-            //第二分身标识
-            $identifier = md5($salt . md5($result['id'] . $salt));
-            //永久登录标识
-            $token = md5(uniqid($result['user_pass'], true));
-            //永久登录超时时间(1周)
-            $timeout = time()+3600*24*7;
-            setcookie('_auth',$identifier.':'.$token,$timeout,"/");
-            $login_data = array(
-                'login_id' => $result['id'],
-                'login_type' => $result['user_type'],
-                'login_ip' => get_client_ip(0,true),
-                'login_time' => date("Y-m-d H:i:s"),
-                'login_token' => $token
-            );
-            $r=M('365class.class_login_log')->add($login_data);
-            $where['id'] =$result["id"];
-            $data = array(
-                'last_login_time' => date("Y-m-d H:i:s"),
-                'last_login_ip' => get_client_ip(0,true),
-                'identifier' =>$identifier,
-                'token'=>$token,
-                'timeout'=>$timeout
-            );
-            $users_model=new Model('365class.class_student_users');
-            $users_model->where($where)->save($data);
-
-            session("user_type",$result["user_type"]);
-            session("nickname", $result["user_nicename"]);
-            session("realname",$result['user_realname']);
-            session("userid", $result["id"]);
-            $sql="select DECODE(mobile,'1!q2@w') from class_student_users where id=".session('userid');
-            $a=new Model();
-            $b=$a->query($sql);
-            $mobile=$b[0]['DECODE(mobile,\'1!q2@w\')'];
-            cookie("_mobile",$mobile);
-            $this->redirect($identify.'/index');
-        }
-    }
-    public function wx_login(){
-        if(IS_POST){
-            $identify=I('post.identify');
-            $appid = "wxb2604be2fd82a0b4";
-            $url="https://open.weixin.qq.com/connect/oauth2/authorize?appid=$appid&redirect_uri=http://wxpay.365xuet.com/test/oauth2.php&response_type=code&scope=snsapi_userinfo&state=$identify";
-            header("Location:".$url);
-        }
-        $this->display();
-    }
-    public function wx_bind(){
-        if(IS_POST){
-            $openid=I('post.openid');
-            $identify=I('post.identify');
-            $mobile = I("post.t_mobile");
-            $passwd = I("post.t_password");
-//            dump($openid);dump($identify);dump($mobile);dump($passwd);
-//            die('here');
-            if (!checkMobile($mobile)) {
-                echo "<script>alert('请输入正确的手机号码');window.history.go(-1);</script>";
-                exit;
-            } else if ($passwd == "") {
-                echo "<script>alert('请输入密码');window.history.go(-1);</script>";
-                exit;
-            }
-            $spasswd = "###" . md5(md5("UewBc27f4YZvbr0e6p" . $passwd));
-            switch ($identify){
-                case 'student':
-                    $users=M('class_student_users');
-                    break;
-                case 'teacher':
-                    $users=M('class_teacher_users');
-                    break;
-            }
-//            dump($user);die;
-            $result=$users->where("mobile=ENCODE('$mobile','1!q2@w') and user_pass='$spasswd'")->save(array('openid'=>$openid));
-//            dump($result);die('here');
-            if($result!=false){
-                $userinfo=$users->where("mobile=ENCODE('$mobile','1!q2@w') and user_pass='$spasswd'")->field('user_type,user_nicename,user_realname,id')->find();
-                if($userinfo){
-                    session('user_type',$userinfo['user_type']);
-                    session('nickname',$userinfo['user_nicename']);
-                    session('realname',$userinfo['user_realname']);
-                    session('userid',$userinfo['id']);
-                    $sql="select DECODE(mobile,'1!q2@w') from class_student_users where id=".session('userid');
-                    $a=new Model();
-                    $b=$a->query($sql);
-                    $mobile=$b[0]['DECODE(mobile,\'1!q2@w\')'];
-                    cookie("_mobile",$mobile);
-                }
-                $url=U('Public/login');
-                $this->show("<script>alert('绑定成功');window.location.href='{$url}';</script>");
-            }else{
-                echo "<script>alert('账户信息不正确');window.history.go(-1);</script>";
-            }
-        }
-        $openid=I('get.openid');
-        $identify=I('get.identify');
-        $this->assign('openid',$openid);
-        $this->assign('identify',$identify);
-        $this->display();
-    }
-    //生成随机数,用于生成salt
-    public function random_str($length){
-        //生成一个包含 大写英文字母, 小写英文字母, 数字 的数组
-        $arr = array_merge(range(0, 9), range('a', 'z'), range('A', 'Z'));
-        $str = '';
-        $arr_len = count($arr);
-        for ($i = 0; $i < $length; $i++){
-            $rand = mt_rand(0, $arr_len-1);
-            $str.=$arr[$rand];
-        }
-        return $str;
-    }
-    public function test5(){
-        $id=3999;
-        $sql="select DECODE(mobile,'1!q2@w') from class_student_users where id='$id'";
-        $a=new Model();
-        $b=$a->query($sql);
-        $mobile=$b[0]['DECODE(mobile,\'1!q2@w\')'];
-        dump($mobile);die;
-        $b=substr($a,7,11);dump($a);
-        dump($b);
     }
 }
